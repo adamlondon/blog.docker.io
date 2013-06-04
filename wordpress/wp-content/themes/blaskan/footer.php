a6af38506bce1aba29066ad2fe64ab05e1fec0e8
Thatcher Peskens <thatcher@dotcloud.com>
2013-06-04 11:37:32 -0700

    modifications to style

---
+++ wordpress/wp-content/themes/blaskan/footer.php
@@ -0,0 +1,18 @@
+		<footer id="footer">
+			<?php echo blaskan_footer_structure(); ?>
+			
+			Docker is a project by <a href="http://www.dotcloud.com">dotCloud</a>
+			<div class="span4 tbox textright forceleftmargin social links">
+                		<a class="twitter" href="http://twitter.com/getdocker">Twitter</a>
+		                <a class="github" href="https://github.com/dotcloud/docker/">GitHub</a>
+			</div>
+		</footer>
+		<!-- / #footer -->
+	</div>
+	<!-- / #wrapper -->
+</div>
+<!-- / #site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
