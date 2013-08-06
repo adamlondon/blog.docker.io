0b74f5833c2648c776d87015058ba4bc936e070f
Thatcher Peskens <thatcher@dotcloud.com>
2013-08-06 11:48:01 -0700

    changed twitter handle from getdocker to docker

--- wordpress/wp-content/themes/blaskan/footer.php
+++ wordpress/wp-content/themes/blaskan/footer.php
@@ -3,7 +3,7 @@
 			
 			Docker is a project by <a href="http://www.dotcloud.com">dotCloud</a>
 			<div class="span4 tbox textright forceleftmargin social links">
-                		<a class="twitter" href="http://twitter.com/getdocker">Twitter</a>
+                		<a class="twitter" href="http://twitter.com/docker">Twitter</a>
 		                <a class="github" href="https://github.com/dotcloud/docker/">GitHub</a>
 			</div>
 		</footer>
